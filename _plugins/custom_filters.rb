cd91ef86b32f162f32d4a1d521603bb07bd1674e
Kevin Colten <kevincolten@gmail.com>
2024-01-05 14:31:50 -0800

    images

--- _plugins/custom_filters.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module CustomTagFilter
-    def image(path)
-      "/assets/images/#{path[18..-4]}.webp"
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::CustomTagFilter)
